c0e677160396265c78d060ad2d32510bd6e8c074
Grant Marsh <gmarsh@mines.edu>
2019-02-21 14:37:33 -0700

    Changed face location and MOER

--- src/Cat.java
+++ src/Cat.java
@@ -8,7 +8,7 @@ public class Cat {
 	private static final int HEAD_DIMENSION = 100;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
-	private static final int EYE_X = HEAD_DIMENSION/4;
+	private static final int EYE_X = (HEAD_DIMENSION/4) + 2;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
 	private static final int EYE_HEIGHT = 20;
@@ -17,8 +17,8 @@ public class Cat {
 	private static final int MOUTH_HEIGHT = 10;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
-	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
+	private static final int MOUTH_X = (HEAD_DIMENSION/5 * 2) - 2;
+	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 2;
 	
 	// draw will render the Cat on the Graphics object
 	public void draw(Graphics g, int catX, int catY)
@@ -44,6 +44,6 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("MOER", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
